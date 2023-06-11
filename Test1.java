317ae8b08b29dc6aec8e0252a6a4da0499ea96d5
deepakkumargfg <deepakkumargfg.1@gmail.com>
2023-06-11 10:52:45 +0530

    initial commit

---
+++ Test1.java
@@ -0,0 +1,32 @@
+class Test1{
+	public static void main(String[] args){
+		int a=5;
+		System.out.println("Value of a: "+a);
+		System.out.println("Size of Integer: "+ Integer.SIZE+" Bits");
+		System.out.println("Minimum of Integer: "+ Integer.MIN_VALUE);
+		System.out.println("Maximum of Integer: "+ Integer.MAX_VALUE);
+
+		short b=6;
+	    System.out.println("Value of b: "+b);
+	    System.out.println("Size of Short: "+ Short.SIZE+" Bits");
+		System.out.println("Minimum of Short: "+ Short.MIN_VALUE);
+		System.out.println("Maximum of Short: "+ Short.MAX_VALUE);
+
+		long c=7;
+		System.out.println("Value of c: "+c);
+		System.out.println("Minimum of Long: "+ Long.MIN_VALUE);
+		System.out.println("Maximum of Long: "+ Long.MAX_VALUE);
+
+		byte d=16;
+		System.out.println("Value of d: "+d);
+		System.out.println("Minimum of Byte: "+ Byte.MIN_VALUE);
+		System.out.println("Maximum of Byte: "+ Byte.MAX_VALUE);
+
+		float e=1f;
+		System.out.println("Value of e: "+e);
+
+		double f1=10.30;
+		System.out.println("Value of e: "+f1);
+
+	}
+}
